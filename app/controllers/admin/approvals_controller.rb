057b12a4dfcb41cd1c5e62f56cd4d9e0bf111796
Bryan Goss <bcgoss@gmail.com>
2017-06-26 16:24:54 -0700

    Add role to user for authorization

---
+++ app/controllers/admin/approvals_controller.rb
@@ -0,0 +1,13 @@
+class Admin::ApprovalsController < AdminController
+  def create
+    @user = User.find(params[:user_id])
+    @user.approved!
+    redirect_to admin_user_path(@user)
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    @user.unverified!
+    redirect_to admin_user_path(@user)
+  end
+end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :user do
     email 'example@example.com'
     password 'password'
-    
+    role 1
   end
 end
