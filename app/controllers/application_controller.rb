2be7a397b1cb9e10149de55f3b2b6563eaec5228
Bryan Goss <bcgoss@gmail.com>
2017-06-26 17:19:00 -0700

    Restrict actions of unverified users
    Restrict actions of unverified users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :authorize_user!
   protect_from_forgery with: :exception
+
+  private
+
+  def authorize_user!
+    !(current_user.unverified?)
+  end
 end
