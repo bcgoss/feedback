98862eabf0a315cf61ee80e02dbb7d919c310927
Bryan Goss <bcgoss@gmail.com>
2017-08-24 11:08:25 -0700

    Add views to lecture

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -11,13 +11,14 @@ class LecturesController < ApplicationController
   def new
     @cohort = Cohort.find(params[:cohort_id])
     @lecture = @cohort.lectures.new
+    @instructors = User.all
   end
 
   def create
     @cohort = Cohort.find(params[:cohort_id])
     @lecture = @cohort.lectures.new(lecture_params)
     if @lecture.save
-      redirect_to cohort_lecture_path @lecture
+      redirect_to cohort_lecture_path @cohort, @lecture
     else
       flash.now[:error] = @lecture.errors.full_messages.join('. ')
       render :new
@@ -28,6 +29,6 @@ class LecturesController < ApplicationController
   private
 
   def lecture_params
-    params.require(:lecture).permit(:start_at, :duration)
+    params.require(:lecture).permit(:start_at, :duration, :user_id, :title)
   end
 end
