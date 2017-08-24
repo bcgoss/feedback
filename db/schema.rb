23d4fc774e01caf3c96654b75e3d1a4549683223
Bryan Goss <bcgoss@gmail.com>
2017-08-24 11:08:04 -0700

    Add title to Lecture

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170705230019) do
+ActiveRecord::Schema.define(version: 20170715181158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170705230019) do
     t.integer  "duration"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "title"
     t.index ["cohort_id"], name: "index_lectures_on_cohort_id", using: :btree
     t.index ["user_id"], name: "index_lectures_on_user_id", using: :btree
   end
